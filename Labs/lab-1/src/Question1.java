0e2594437388edc679ed6fd227bcdfc4a17d5b72
bChandrax <bchandrax65@gmail.com>
2026-08-29 09:10:02 +0200

    Question 1. Personal Profile

---
+++ Labs/lab-1/src/Question1.java
@@ -0,0 +1,15 @@
+public class Question1 {
+    public static void main(String[] args) throws Exception {
+        //Define and assign
+        String name = "Karabo Machanja";
+        int age = 20;
+        double GPA = 3.6;
+        boolean isFulltime = true;
+
+        //printing statement
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
+        System.out.println("GPA: "+ GPA);
+        System.out.println("Fulltime: "+ isFulltime);
+    }
+}
