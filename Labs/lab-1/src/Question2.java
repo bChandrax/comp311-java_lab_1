e3b3799f4d060b834e1d8bcd92ad61c8f10c9239
bChandrax <bchandrax65@gmail.com>
2026-08-29 09:10:20 +0200

    Question 2. Primitive Playground

---
+++ Labs/lab-1/src/Question2.java
@@ -0,0 +1,23 @@
+public class Question2 {
+    public static void main(String[] args) throws Exception {
+        //definition and assignments
+        byte testOne = 100;
+        short testTwo = 20000;
+        int testThree = 350000;
+        long testFour = 900000000;
+        float testFive = 3.14f;
+        double testSix = 3.14159265;
+        char testSeven = 'J';
+        boolean testEight = true;
+
+        //printing statements
+        System.out.println("byte: " + testOne);
+        System.out.println("short: "+ testTwo);
+        System.out.println("int: " + testThree);
+        System.out.println("long: "+ testFour);
+        System.out.println("float: " + testFive);
+        System.out.println("double: " + testSix);
+        System.out.println("char: " + testSeven);
+        System.out.println("boolean: " + testEight);
+    }
+}
