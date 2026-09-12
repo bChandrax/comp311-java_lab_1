e32517fe1962c60ed4e7c630ed77ca98a0f71c66
bChandrax <bchandrax65@gmail.com>
2026-09-12 13:17:43 +0200

    Question 3 — Even Numbers

---
+++ Lab3/src/Question3.java
@@ -0,0 +1,17 @@
+public class Question3 {
+    public static void main(){
+        //initialise count
+        int count = 1;
+
+        //loop while count is less than or equal to 50
+        while(count<=50){
+
+            //if the number is even
+            if (count%2==0){
+                //print the number
+                System.out.println(count);
+            }
+            count++;
+        }
+    }
+}
