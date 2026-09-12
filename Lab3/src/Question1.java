5538d46d889ed29043a0a10307ebd0ed167338fe
bChandrax <bchandrax65@gmail.com>
2026-09-12 13:16:57 +0200

    Question 1 — Countdown

---
+++ Lab3/src/Question1.java
@@ -0,0 +1,14 @@
+public class Question1 {
+    public static void main(String[] Args){
+        //initialise count
+        int count=10;
+
+        //while count is more than 1 or equal to 1
+        while(count>=1){
+            System.out.println(count);
+            count--;
+        }
+
+        System.out.println("LIFT OFF!!");
+    }    
+}
