761aa909b2c196a9f1e45dac707def7455d77738
bChandrax <bchandrax65@gmail.com>
2026-09-19 14:04:10 +0200

    Question 1 — Declaring and Initialising an Array

---
+++ Lab4-pt2/src/Question1.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Question1{
+    public static void main(String[] Args)throws Exception{
+        //initialising array
+        int[] dailyTemperatures = {23,15,32,56,41,44,-2};
+
+        //print them out
+        System.out.println(Arrays.toString(dailyTemperatures));
+    }
+}
