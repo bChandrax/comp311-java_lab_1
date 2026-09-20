d95ad3655e535e4c28e43afe93ae73052bb44c02
bChandrax <bchandrax65@gmail.com>
2026-09-20 09:36:50 +0200

    Question 4 — Adding a Method

---
+++ lab5/src/Question4.java
@@ -0,0 +1,11 @@
+public class Question4{
+    public  static void main(String[] Args){
+        //the getSummary() method os defined in 
+
+        //defining the book to use the method on
+        Book myBook = new Book("Just forget about it","idk i forgot",8.99);
+
+        //using the method
+        myBook.getSummary();
+    }
+}
