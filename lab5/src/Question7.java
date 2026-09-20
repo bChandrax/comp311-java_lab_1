bf563e7032e14d70e2e647980d5e34c2631ee81f
bChandrax <bchandrax65@gmail.com>
2026-09-20 09:39:24 +0200

    Question 7 — A Second Class

---
+++ lab5/src/Question7.java
@@ -0,0 +1,1 @@
+//go to Student.java
---
+++ lab5/src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+
+    //variable declaration
+    private String name;
+    private int age;
+    private double gpa;
+
+    //constructor
+    Student(){}
+
+    //getters
+    public String getName(){return name;}
+    public int getAge(){return age;}
+    public double getGpa(){return gpa;}
+
+    //setters
+    public void setName(String name){this.name=name;};
+    public void setAge(int age){this.age=age;};
+    public void setGpa(double gpa){this.gpa=gpa;};
+
+}
